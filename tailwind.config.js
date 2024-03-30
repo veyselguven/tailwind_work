9b92167ed08f547cf148dc9686dc3d9bcfcc42fb
veyselguven <vgbasbaydar@gmail.com>
2024-03-29 18:28:15 -0700

    added some font and some theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        genumu: ["Gemunu Libre", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        "gega-red": "#BC1A45",
+        "gega-melon": "#FFD369",
+        "gega-grey": "#DDDDDD",
+        "gega-white": "#FZFZFZ",
+      },
+    },
   },
   plugins: [],
 };
